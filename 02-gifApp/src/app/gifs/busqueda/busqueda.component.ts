84b3ed5109793ebc34f832d018fbce952fa41bfd
saul <saul@.hotmail.com>
2022-04-18 17:43:15 -0500

    viewChild

--- 02-gifApp/src/app/gifs/busqueda/busqueda.component.ts
+++ 02-gifApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent  {
 
   constructor() { }
 
-  ngOnInit(): void {
+  @ViewChild('txtBuscar')txtBuscar!:ElementRef<HTMLInputElement>;
+
+  buscar(){
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value="";
+    
   }
+  
 
 }
