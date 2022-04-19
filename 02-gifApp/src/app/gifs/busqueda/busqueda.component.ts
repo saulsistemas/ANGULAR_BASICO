68c00473a72eee57b5eb301c9dce721ded31c738
saul <saul@.hotmail.com>
2022-04-19 11:06:40 -0500

    Listar gifs

--- 02-gifApp/src/app/gifs/busqueda/busqueda.component.ts
+++ 02-gifApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -7,13 +8,13 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class BusquedaComponent  {
 
-  constructor() { }
+  constructor(private gifsService:GifsService) { }
 
   @ViewChild('txtBuscar')txtBuscar!:ElementRef<HTMLInputElement>;
 
   buscar(){
     const valor = this.txtBuscar.nativeElement.value;
-    console.log(valor);
+    this.gifsService.buscarGifs(valor)
     this.txtBuscar.nativeElement.value="";
     
   }
