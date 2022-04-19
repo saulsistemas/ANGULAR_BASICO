9bdf61da48a7812b10f8fe2242f4ba90781f0188
saul <saul@.hotmail.com>
2022-04-19 15:40:04 -0500

    params

--- 02-gifApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifApp/src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SearchGifsResponse, Gif } from '../interface/gifs.inteface';
 
@@ -7,7 +7,8 @@ import { SearchGifsResponse, Gif } from '../interface/gifs.inteface';
 })
 export class GifsService {
 
-  private apikey:string='fNel52976UIKB0L8EIiPKyONZNIKpKKg'
+  private apikey:string='fNel52976UIKB0L8EIiPKyONZNIKpKKg';
+  private servicioUrl:string='https://api.giphy.com/v1/stickers';
   private _historial:Array<string> = [];
   public resultados:Array<Gif> =[];
   get historial(){
@@ -27,8 +28,12 @@ export class GifsService {
       //console.log(this._historial);
       localStorage.setItem('historial',JSON.stringify(this._historial))
     }
+    const params = new HttpParams()
+    .set('api_key',this.apikey)
+    .set('limit','10')
+    .set('q',query);
     //LA RESPUESTA LUCE COMO ESA INTERFACE
-    this._http.get<SearchGifsResponse>(`https://api.giphy.com/v1/stickers/search?api_key=fNel52976UIKB0L8EIiPKyONZNIKpKKg&q=${query}&limit=10`)
+    this._http.get<SearchGifsResponse>(`${this.servicioUrl}/search`,{params:params})
     .subscribe((response:SearchGifsResponse)=>{
         this.resultados = response.data;
         console.log(this.resultados);
