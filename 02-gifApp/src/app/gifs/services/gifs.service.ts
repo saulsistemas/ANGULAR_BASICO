87ab9b1cb686ef411b86cbfb8b5b2418f0c1f9f6
saul <saul@.hotmail.com>
2022-04-19 14:11:13 -0500

    Modulo HttpClientModule para poder conectarnos a una api mediante ruta

--- 02-gifApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifApp/src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,10 +6,16 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apikey:string='fNel52976UIKB0L8EIiPKyONZNIKpKKg'
   private _historial:Array<string> = [];
+  private url:string="https://api.giphy.com/v1/stickers/search?api_key=fNel52976UIKB0L8EIiPKyONZNIKpKKg&q=dragon%20ball%20z&limit=10";
 
   get historial(){
     return [...this._historial];
+  }
+
+  constructor(private _http:HttpClient){
+
   }
 
   buscarGifs(query:string){
@@ -16,8 +23,13 @@ export class GifsService {
     if (!this._historial.includes(query)) {//PARA NO VOLVER A BUSCAR
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
-      console.log(this._historial);
+      //console.log(this._historial);
     }
+
+    this._http.get(this.url).subscribe((response:any)=>{
+      console.log(response.data);
+      
+    })
     
   }
 }
