509dff575cd8216ca0502eb2920191c620c26f28
saul <saul@.hotmail.com>
2022-04-19 17:34:24 -0500

    configurando rutas

--- 03-paisesApp/src/app/app.module.ts
+++ 03-paisesApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { PaisModule } from './pais/pais.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { PaisModule } from './pais/pais.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     SharedModule,
     PaisModule,
   ],
