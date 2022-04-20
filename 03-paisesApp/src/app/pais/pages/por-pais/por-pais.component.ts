7b66292b077380bc466fd2f22101d966e096de99
saul <saul@.hotmail.com>
2022-04-20 10:55:21 -0500

    FormModule y ngModel busvar formulario

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,11 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent  {
 
+  termino:string="";
   constructor() { }
 
-  ngOnInit(): void {
-  }
+ buscar(){
+   console.log(this.termino);
+   
+ }
 
 }
