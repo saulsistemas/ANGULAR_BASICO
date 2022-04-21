6dae754b163b99385411e45472ea0d4e6674ee5d
saul <saul@.hotmail.com>
2022-04-21 14:06:32 -0500

    Tabla e Input para recibir datos desde otro componente

---
+++ 03-paisesApp/src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html',
+  styles: [
+  ]
+})
+export class PaisInputComponent  {
+  termino:string="";
+
+  constructor() { }
+
+  buscar(){}
+}
