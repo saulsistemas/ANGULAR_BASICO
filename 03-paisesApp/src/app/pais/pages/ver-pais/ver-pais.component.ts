6ba9cf0670cfaaa7d527781c62544576c0d3f45c
saul <saul@.hotmail.com>
2022-04-21 16:30:13 -0500

    rxjs switchmap ver por observable

--- 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -14,14 +15,23 @@ export class VerPaisComponent implements OnInit {
 
   ngOnInit(): void {  
     this.activatedRoute.params
-      .subscribe( ({id})=>{//params:any
-        console.log(id); //BIENE DE LA URL DEFINIDO EN ROUTER
-        this.paisService.getPaisId(id)
-          .subscribe(pais=>{
-            console.log(pais);
-            
-          })
+      .pipe(
+        switchMap(({id})=>
+          this.paisService.getPaisId(id)
+        )
+      )
+      .subscribe( resp=>{//params:any
+        console.log(resp);
+        
       })
+    //this.activatedRoute.params
+    //  .subscribe( ({id})=>{//params:any
+    //    console.log(id); //BIENE DE LA URL DEFINIDO EN ROUTER
+    //    this.paisService.getPaisId(id)
+    //      .subscribe(pais=>{
+    //        console.log(pais);
+    //      })
+    //  })
   }
 
 }
