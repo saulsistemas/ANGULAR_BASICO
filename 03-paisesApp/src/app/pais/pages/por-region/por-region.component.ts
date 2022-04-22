93a17c2a79c9c2a17556f543058ff81ab1893458
saul <saul@.hotmail.com>
2022-04-22 11:08:37 -0500

    ngClass , [class] class

--- 03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -4,13 +4,24 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
+    `button{
+      margin-right:5px;
+    }`
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent  {
 
+  regiones:Array<string> =['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva:string='';
   constructor() { }
 
-  ngOnInit(): void {
+  activarRegion(region:string){
+    this.regionActiva = region;
+    console.log(this.regionActiva);
+    
   }
 
+  getClaseCss(region:string){
+    return (region == this.regionActiva) ? 'btn-primary':'btn-btn-outline-primary'
+  }
 }
