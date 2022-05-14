806360a6ea25647b7f5c28c26d92432be7b93b9d
Laptop Toshiba <santamariaramos18@hotmail.com>
2022-05-13 21:21:02 -0500

    creando rutas, exportando

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 
@@ -12,6 +13,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     SharedModule,
+    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
