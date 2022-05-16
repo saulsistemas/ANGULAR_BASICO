4f3e16fd365d3ec0bc2fcbb682f69cee9ea85c5b
Laptop Toshiba <santamariaramos18@hotmail.com>
2022-05-16 00:02:45 -0500

    i18nSelect

--- 04-pipesApp/src/app/app.component.ts
+++ 04-pipesApp/src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   nombre:string='Saul SaNTamaria';
   valor:number=1000;
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
   obj={
     nombre:this.nombre,
   }
--- 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { PrimeNGConfig } from 'primeng/api';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-no-comunes',
@@ -7,12 +7,14 @@ import { PrimeNGConfig } from 'primeng/api';
   styles: [
   ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent  {
 
-  constructor(private primengConfig: PrimeNGConfig) {}
+  nombre:string = "Saul";
+  genero:string="masculino";
 
-  ngOnInit() {
-    this.primengConfig.ripple = true;
+  invitacionMap={
+    'masculino':'inviarlo',
+    'femenino':'inviarla',
+
   }
-
 }
