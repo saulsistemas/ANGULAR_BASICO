e45a732ee882b3c8be0034253386bad7ffd7c72c
Laptop Toshiba <santamariaramos18@hotmail.com>
2022-05-15 22:16:59 -0500

    pipe decimal

--- 04-pipesApp/src/app/ventas/pages/numeros/numeros.component.ts
+++ 04-pipesApp/src/app/ventas/pages/numeros/numeros.component.ts
@@ -6,11 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class NumerosComponent implements OnInit {
+export class NumerosComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ventasNetas:number =2467789.5567;
+  porcentaje:number =0.48;
 }
